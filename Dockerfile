51f5ad575337d13b0264be95f6f438f49eb2283f
John Thompson <john@ufmedia.co.uk>
2022-02-11 10:24:56 +0000

    Init

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM node:14
+
+WORKDIR /var/www/html/wp-content/themes/horizon/gulp
+
---
+++ www/wp-content/themes/horizon/js/app.js
@@ -0,0 +1,10 @@
+(() => {
+  // resources/js/app.js
+  window.addEventListener("load", function() {
+    let main_navigation = document.querySelector("#primary-menu");
+    document.querySelector("#primary-menu-toggle").addEventListener("click", function(e) {
+      e.preventDefault();
+      main_navigation.classList.toggle("hidden");
+    });
+  });
+})();
