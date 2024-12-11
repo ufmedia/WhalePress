4f1a286fee2a0064cf6b46886a3cae032e0def31
John Thompson <john.thompson@affinity-digital.com>
2024-12-11 20:23:26 +0000

    fix: correct text in home page header

--- cypress/e2e/frontend/site.home.cy.js
+++ cypress/e2e/frontend/site.home.cy.js
@@ -1,6 +1,6 @@
 describe("Loading the site (Home Page)", () => {
   it("visits the home page", () => {
     cy.visitHost();
-    cy.get("h1").contains("DDevelop WordPress sites faster.");
+    cy.get("h1").contains("Develop WordPress sites faster.");
   });
 });
